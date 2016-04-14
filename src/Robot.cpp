f11363381cb871ffd4055daf52d3a5ae40d8e242
willgebhardt <William.Gebhardt@asdnh.org>
2016-04-14 08:36:44 -0400

    First commit

---
+++ src/Robot.cpp
@@ -0,0 +1,45 @@
+/*
+ * Robot.cpp
+ *
+ *  Created on: Apr 14, 2016
+ *      Author: Will
+ */
+
+#include "WPILib.h"
+
+class Robot;
+/**
+ * This is a demo program showing how to use Mecanum control with the driveTrain class.
+ */
+class Robot: public SampleRobot
+{   // robot drive system
+
+
+
+public:
+   Robot(){
+
+   }
+   void OperatorControl()
+   {
+   }
+
+};
+
+
+START_ROBOT_CLASS(Robot);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
